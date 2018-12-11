ed511cae68f153aa8ae9b69b2528635283fd9a3f
olegush <43958767+olegush@users.noreply.github.com>
2018-12-11 11:58:42 +0300

    added the form handling

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from typograph import typograph
+
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/typograph', methods=['GET', 'POST'])
 def form():
-    return render_template('form.html')
+    if request.method == 'POST':
+        source_text = request.form['text']
+        result_text = typograph(request.form['text'])
+    else:
+        source_text = ''
+        result_text = ''
+    return render_template(
+        'form.html',
+        source_text=source_text,
+        result_text=result_text
+    )
+
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
